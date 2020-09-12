7b26fee5fb5e707cc4065f11cbdf439c69e763ab
Andreas <anondod3@gmail.com>
2020-09-12 15:30:17 +0200

    Static image memory

--- raytracer/raytracer.cpp
+++ raytracer/raytracer.cpp
@@ -2,20 +2,21 @@
 
 int main()
 {
-    const int height = 361;
-    const int width = 867;
-    unsigned char image[height][width][BYTES_PER_PIXEL];
+    printf("start");
+    const int height = 800;
+    const int width = 800;
+    static unsigned char image[height][width][BYTES_PER_PIXEL];
     char* imageFileName = (char*)"result.bmp";
 
     int i, j;
     for (i = 0; i < height; i++) {
         for (j = 0; j < width; j++) {
-            image[i][j][2] = (unsigned char)(i * 255 / height);             ///red
-            image[i][j][1] = (unsigned char)(j * 255 / width);              ///green
-            image[i][j][0] = (unsigned char)((i + j) * 255 / (height + width)); ///blue
+            image[i][j][2] = (unsigned char)(i);            ///red
+            image[i][j][1] = (unsigned char)(j);              ///green
+            image[i][j][0] = (unsigned char)(j+i);              ///blue
         }
     }
-
+    printf("Generating...  ");
     generateBitmapImage((unsigned char*)image, height, width, imageFileName);
     printf("Image generated!!");
 }
