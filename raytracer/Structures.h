6635c95719531daa2156563346aec81ce0275827
Andreas <anondod3@gmail.com>
2020-09-21 12:20:01 +0200

    1-layer raycast + rendering kind of works

---
+++ raytracer/Structures.h
@@ -0,0 +1,252 @@
+#pragma once
+#include <string>
+#include <math.h>
+#include <vector>
+#include <iostream>
+
+
+class Vector3 {
+public:
+    double x;
+    double y;
+    double z;
+    Vector3()
+    {
+        x = 0.0;
+        y = 0.0;
+        z = 0.0;
+	}
+    Vector3(double in_x, double in_y, double in_z)
+    {
+        x = in_x;
+        y = in_y;
+        z = in_z;
+    }
+
+    Vector3& operator+=(const Vector3 inV)
+    {
+        x = x + inV.x;
+        y = y + inV.y;
+        z = z + inV.z;
+
+        return *this;
+	}
+
+    friend Vector3 operator+(Vector3 lhs, const Vector3& rhs)
+    {
+        lhs += rhs;
+        return lhs;
+	}
+
+    Vector3& operator-=(const Vector3 inV)
+    {
+        x = x - inV.x;
+        y = y - inV.y;
+        z = z - inV.z;
+
+        return *this;
+	}
+
+    friend Vector3 operator-(Vector3 lhs, const Vector3& rhs)
+    {
+        lhs -= rhs;
+        return lhs;
+	}
+
+    double abs()
+    {
+        return sqrt(x * x + y * y + z * z);
+    }
+
+    Vector3 normalize()
+    {
+        return (*this).mult(sqrt(x * x + y * y + z * z));
+    }
+
+    double dot(Vector3 inV)
+    {
+        return x * inV.x + y * inV.y + z * inV.z;
+    }
+    Vector3 cross(Vector3 inV)
+    {
+        return Vector3(y * inV.z - inV.y* z, z * inV.x - inV.z * x, x * inV.y - inV.x * y);
+    }
+
+    Vector3 mult(double factor)
+    {
+        return Vector3(factor * x, factor * y, factor * z);
+	}
+    std::string to_string()
+    {
+        std::string out_string = "<";
+        out_string += std::to_string(x) + ", "
+            + std::to_string(y) + ", "
+            + std::to_string(z) + ">";
+
+        return out_string;
+	}
+    friend std::ostream& operator<<(std::ostream& os, Vector3& inV)
+    {
+        os << inV.to_string();
+        return os;
+    }
+};
+
+
+
+struct Ray {
+    Vector3 start_point;
+    Vector3 end_point;
+    Vector3 ray_color;
+};
+
+const int DIFFUSE = 0;
+const int MIRROR = 1;
+const int GLASS = 2;
+
+
+
+
+struct Triangle{
+public:
+    Vector3 v0;
+    Vector3 v1;
+    Vector3 v2;
+
+    Vector3 normal;
+
+    Vector3 color;
+
+    int material;
+
+    Triangle() = delete;
+	
+    Triangle(const Vector3& in0, const Vector3& in1, const Vector3& in2, const Vector3& inc, int inm)
+    {
+        v0 = in0;
+        v1 = in1;
+        v2 = in2;
+        color = inc;
+        material = inm;
+
+        Vector3 side1 = v1 - v0;
+        Vector3 side2 = v2 - v0;
+
+        normal = side1.cross(side2);
+	}
+
+    float rayIntersection(Ray &r)
+    {
+        Vector3 T = r.start_point - v0;
+        Vector3 E1 = v1 - v0;
+        Vector3 E2 = v2 - v0;
+        Vector3 D = r.end_point - r.start_point;
+        Vector3 P = D.cross(E2);
+        Vector3 Q = T.cross(E1);
+
+        Vector3 tuv = Vector3(Q.dot(E2), P.dot(T), Q.dot(D)).mult(1.0 / P.dot(E1));
+
+        // if ray intersected triangle return t, else return -1 for miss
+        if (tuv.y >= 0 && tuv.z >= 0 && tuv.y + tuv.z <= 1)
+            return tuv.x;
+        else
+            return -1.0;
+    }
+};
+
+std::vector<Triangle> triangles;
+
+
+class Camera {
+public:
+    Vector3 camera_position;
+    double offset = 1.0;
+
+    Vector3 view_direction;
+
+
+    static const int width = 800;
+    Vector3 screen[width][width];
+
+    double  pixel_size = 1.0 / (width / 2.0);;
+
+    Camera() 
+    {
+       //fill screen with zero
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < width; j++) {
+                screen[i][j] = Vector3(); // zero vector
+            }
+        }
+    }
+
+    // xy image plane, not 3d space coordinates
+    Vector3 calculate_pixel_color(int x, int y) 
+    {
+        // calculate pixel-plane position
+        Vector3 pixel_position = camera_position + Vector3(offset, (y - width/2) * pixel_size, (x - width/2) * pixel_size);
+        //ABOVE PIXEL COORDS MIGHT BE WRONG
+
+        // Send ray towards position
+        Ray r;
+        r.end_point = pixel_position;
+        r.start_point = camera_position;
+        
+
+        // calculate closest triangle hit
+        int triangle_index = -1;
+        float min_t = 1000000000;
+        for (int i = 0; i < triangles.size(); i++) {
+            float temp_t = triangles[i].rayIntersection(r);
+            if (temp_t > 0.0001 && temp_t < min_t) {
+                min_t = temp_t;
+                triangle_index = i;
+            }
+        }
+
+        //TEMP return triangle color if hit or black if miss
+        if (min_t > 0.0001 && triangle_index != -1) 
+            return triangles[triangle_index].color;
+        else
+            return Vector3();
+    }
+
+    // xy image plane, not 3d space coordinates
+    void render_pixel(int x, int y, Vector3 color) 
+    {
+        screen[x][y] = color;
+    }
+};
+
+
+/*
+ColorDbl getLightFromDirection(Vertex start, Vertex dir) {
+    Ray r = raytrace(start, dir);
+    hitpos;
+    hitriangle
+    hitriangle.normal
+
+    Direction reflectiondir = reflect(r.end, r.hittriangle.normal);
+    Direction refractiondir = refract(r.end, r.hittriangle);
+
+    color += hitriangle.color * getDiffuseLight(r, lightlist); //shadow rays
+    // point light position + n | dir2light
+
+
+    color += hitriangle.specular_color * getLightFromDirection(r, reflectiondir); //reflection
+    color += hitriangle.color * getLightFromDirection(r, refractiondir); //refraction
+
+    return color;
+}
+
+ColorDbl getDiffuseLight(Vertex hit_position, Triangle inT) {
+
+    for (light_source in light_source_list) {
+        raytrace(hit_position, light_source.position);
+
+        hitpoint->ljus->triangel  if (t < 1)
+    }
+}
+
+ColorDbl color = getLightFromDirection(start, dir, importance, count_bounces);
+*/
--- raytracer/raytracer.cpp
+++ raytracer/raytracer.cpp
@@ -1,52 +1,86 @@
 #include "BmpSave.h"
+#include "Structures.h"
 
 #include <thread>
 #include <array>
+#include <vector>
 #include <iostream>
 #include <mutex>
 
 const int n_threads = 4;
 
-const int height = 800;
 const int width = 800;
 char* imageFileName = (char*)"result.bmp";
 
 const int drawinterval = 20; // percentage
 
 
-static unsigned char image[height][width][BYTES_PER_PIXEL];
+static unsigned char image[width][width][BYTES_PER_PIXEL];
 
-int drawmod = drawinterval / 100.0f * width * height;
+int drawmod = drawinterval / 100.0f * width * width;
 int drawnpercentage = 0;
 int drawnpixels = 0;
 
 std::mutex mtx;
+
+std::vector<Triangle> scene;
+
+Camera camera;
+
+
 
 //bad wait function
 void badwait() {
     for (int i = 0; i < 1000000000; i++);
 }
 
+void drawImageToBMP() {
+    //screen = onormaliserad;
+    //find max och min
+    //screen/max
+    
+    double imax = 0;
+    for (int y = 0; y < width; y++) {
+        for (int x = 0; x < width; x++) {
+            if (imax < camera.screen[x][y].x)
+                imax = camera.screen[x][y].x;
+            if (imax < camera.screen[x][y].y)
+                imax = camera.screen[x][y].y;
+            if (imax < camera.screen[x][y].z)
+                imax = camera.screen[x][y].z;
+        }
+    }
+    for (int y = 0; y < width; y++) {
+        for (int x = 0; x < width; x++) {
+            //draw pixel
+            image[x][y][2] = (unsigned char)(camera.screen[x][y].x * 255.99 / imax);            ///red
+            image[x][y][1] = (unsigned char)(camera.screen[x][y].y * 255.99 / imax);            ///green
+            image[x][y][0] = (unsigned char)(camera.screen[x][y].z * 255.99 / imax);            ///blue
+        }
+    }
+
+    generateBitmapImage((unsigned char*)image, width, width, imageFileName);
+    drawnpercentage += drawinterval;
+    std::cout << drawnpercentage << "% rendered\n";
+    //badwait(); //for testing
+}
+
 // render a vertical segment of the image, placeholder rgb values instead of raytraced values
 void rendersegment(int starty, int endy, int r, int g, int b) {
-    for (int i = starty; i < endy; i++) {
-        for (int j = 0; j < width; j++) {
+    // xy for 2d image, not representative of actual 3d-space
+    for (int y = starty; y < endy; y++) {
+        for (int x= 0; x < width; x++) {
             //decide pixel color here (aka do raytracing)
+            Vector3 color = camera.calculate_pixel_color(x, y);
 
             //draw pixel in image and ocasionally render to bmp
             mtx.lock();
             {
-                //draw pixel
-                image[i][j][2] = (unsigned char)(r);            ///red
-                image[i][j][1] = (unsigned char)(g);            ///green
-                image[i][j][0] = (unsigned char)(b);            ///blue
+                camera.render_pixel(x, y, color);
 
                 //render to bmp in interval
                 if (++drawnpixels % drawmod == 0) {
-                    generateBitmapImage((unsigned char*)image, height, width, imageFileName);
-                    drawnpercentage += drawinterval;
-                    std::cout << drawnpercentage << "% rendered\n";
-                    badwait(); //for testing
+                    drawImageToBMP();
                 }
             }
             mtx.unlock();
@@ -54,22 +88,48 @@ void rendersegment(int starty, int endy, int r, int g, int b) {
     }
 }
 
+
+
 int main()
 {
+
+    Vector3 vertex1 = Vector3(4.0, -1.0, -1.0);
+    Vector3 vertex2 = Vector3(4.0, -1.0, 1.0);
+    Vector3 vertex3 = Vector3(4.0, 1.0, 0.0);
+    Vector3 color = Vector3(0.8, 0.6, 0.1);
+    Triangle test_triangle = Triangle(vertex1, vertex2, vertex3, color, 0);
+
+    triangles.push_back(test_triangle);
+
+
+    /*
+    Vector3 test_vector_add = test_vector1 + test_vector2;
+    Vector3 test_vector_cross = test_vector1.cross(test_vector2);
+    test_vector2 += test_vector1;
+    std::cout << test_vector1<<"\n";
+    std::cout << test_vector2<<"\n";
+    
+    
+    std::cout << "Addition" << test_vector_add<< "\n";
+    std::cout << "+=" <<test_vector2.to_string()<< "\n";
+    std::cout << "cross" <<test_vector_cross.to_string()<< "\n"; // 0 -1 0
+    std::cout << "dot product (1.0, 2.0, 3.0) dot  (0.0, 1.0, 2.0) = " << Vector3(1.0, 2.0, 3.0).dot(Vector3(0.0, 1.0, 2.0)) << "\n";
+    */
+
     std::array<std::thread, n_threads> threads;
     for (int i = 0; i < n_threads; i++) {
-        int start = i * height / n_threads;
-        int end = (i + 1) * height / n_threads;
+        int start = i * width / n_threads;
+        int end = (i + 1) * width / n_threads;
         std::cout << "thread " << i << " assigned to height " << start << "-" << end << "\n";
         threads[i] = std::thread(rendersegment, start, end, 0, 0, i * 255 / n_threads);
     }
 
     std::cout << "Rendering started:\n";
-        
+
     for (int i = 0; i < n_threads; i++)
         threads[i].join();
 
-    generateBitmapImage((unsigned char*)image, height, width, imageFileName);
+    generateBitmapImage((unsigned char*)image, width, width, imageFileName);
     std::cout << "Image completed.";
 }
 
